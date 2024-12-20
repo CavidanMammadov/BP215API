06ce69bb4cf3f3c89b0681eccd01b8bfef963d76
CavidanMammadov <cavidanbm-bp215@code.edu.az>
2024-12-20 17:56:10 +0400

    Added working for pc

---
+++ BP215API/DAL/BP215APIDbContext.cs
@@ -0,0 +1,38 @@
+﻿
+
+using BP215API.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace BP215API.DAL
+{
+    public class BP215APIDbContext : DbContext
+    {
+        public BP215APIDbContext(DbContextOptions options) : base(options)
+        { }
+        public DbSet<Language> Languages { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Language>(b =>
+            {
+                b.HasKey(x => x.Code);
+                b.Property(x => x.Code)
+                .IsFixedLength(true)
+                .HasMaxLength(2);
+                b.HasIndex(x => x.Name)
+                .IsUnique();
+                b.Property(x => x.Name)
+                .IsRequired().HasMaxLength(32);
+                b.Property(x => x.Icon).HasMaxLength(128);
+                b.HasData(new Language
+                {
+                    Code ="az",
+                    Name = "Azərbaycan",
+                    Icon = "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Flag_of_Azerbaijan.svg/2560px-Flag_of_Azerbaijan.svg.png"
+                });
+            });
+            base.OnModelCreating(modelBuilder);
+        }
+
+
+    }
+}
---
+++ BP215API/Migrations/20241220135243_SeedTadasForTable.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BP215API.Migrations
+{
+    /// <inheritdoc />
+    public partial class SeedTadasForTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Languages",
+                columns: new[] { "Code", "Icon", "Name" },
+                values: new object[] { "az", "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Flag_of_Azerbaijan.svg/2560px-Flag_of_Azerbaijan.svg.png", "Azərbaycan" });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Languages",
+                keyColumn: "Code",
+                keyValue: "az");
+        }
+    }
+}
